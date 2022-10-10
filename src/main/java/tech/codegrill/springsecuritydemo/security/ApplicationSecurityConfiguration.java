dbe577ce893f5d0e422d199a03f2211e116a9463
Andreas MOUZONG <andreasmouzong@gmail.com>
2022-10-10 12:38:03 +0200

    actualising the InMemoryUserDetailsManager

--- src/main/java/tech/codegrill/springsecuritydemo/security/ApplicationSecurityConfiguration.java
+++ src/main/java/tech/codegrill/springsecuritydemo/security/ApplicationSecurityConfiguration.java
@@ -1,9 +1,14 @@
 package tech.codegrill.springsecuritydemo.security;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
 @Configuration
 @EnableWebSecurity
@@ -12,10 +17,29 @@ public class ApplicationSecurityConfiguration extends WebSecurityConfigurerAdapt
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
-                .authorizeRequests()
-                .anyRequest()
-                .authenticated()
+                .authorizeRequests() //authorize every request fro client
+                .antMatchers("/", "index", "/css/*", "/js/*") // permit users to access these resources with or without authorizations
+                .permitAll()// permit the access to all of these resources
+                .anyRequest()// Allow any request to enter the server
+                .authenticated() // request must be authenticated
                 .and()
-                .httpBasic();
+                .httpBasic(); // use basic authentication (pop-up auth from the browser )
+    }
+
+    @Override
+    @Bean
+    protected UserDetailsService userDetailsService() {
+        /**
+         * Determining how we retrieve user information from our database
+         *
+         */UserDetails andreasFefe = User.builder()
+                .username("andreas")
+                .password("testPassword")
+                .roles("ADMIN") // ROLE_ADMIN
+                .build();
+
+        return new InMemoryUserDetailsManager(
+                andreasFefe
+        );
     }
 }
